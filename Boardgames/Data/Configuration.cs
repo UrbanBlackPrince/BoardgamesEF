435ea008db6c143cd4e05ee4e4690939810f9b3f
Gabriel Nikolov <urbanblackprince@gmail.com>
2023-09-17 13:20:01 +0300

    Add project files.

---
+++ Boardgames/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Boardgames.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = "Server=DESKTOP-4LF9R5N\\SQLEXPRESS02;Database=Boardgames;Integrated Security=True;Encrypt=False";
+    }
+}
